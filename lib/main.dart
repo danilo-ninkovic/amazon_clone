f8caefdba88e75726b78a8c777e5e2ca01af515c
danilo-ninkovic <ninkovicdanilo@hotmail.com>
2022-11-21 22:34:00 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(),
+      home: const Text('Flutter Demo Home Page'),
+    );
+  }
+}
